298b07ba823c2bd2a5340961edbc0bc7e54e91ae
akshat.joshi <akshatjoshi52@gmail.com>
2020-04-21 10:09:58 +0000

    added greeting resource

---
+++ demo/src/main/java/com/example/demo/greeting/GreetingApplication.java
@@ -0,0 +1,9 @@
+package com.example.demo.greeting;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/greeting/")
+public class GreetingApplication extends Application{
+
+}
